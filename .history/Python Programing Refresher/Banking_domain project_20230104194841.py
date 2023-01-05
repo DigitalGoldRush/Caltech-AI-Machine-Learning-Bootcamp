9ba135d0e933188d6c1af10723016e8f98e3c80d
Michael <michael@digitalgoldrush.co>
2023-01-04 20:01:17 -0800

    update

---
+++ .history/Python Programing Refresher/Banking_domain project_20230104194841.py
@@ -0,0 +1,8 @@
+#Use parameterized constructors in all classes to initialize default values
+#Create a bank class with the following attributes:
+
+#IFSC_Code
+#Bank_Name
+#Branch_Name
+#Location
+
---
+++ Python Programing Refresher/Banking_domain project.py
@@ -0,0 +1,40 @@
+#Use parameterized constructors in all classes to initialize default values
+#Create a bank class with the following attributes:
+#IFSC_Code
+#Bank_Name
+#Branch_Name
+#Location
+
+#Create a customer class with the following attributes:
+#customerID
+#custname
+#address
+#contactdetails
+
+#Create an account class that inherits from the bank class 
+# the following attributes (use Super() to pass value to the base class):
+#AccountID
+#Cust Object of Customer
+#balance
+
+
+#Add the following methods to get account information, withdraw, and deposit:
+#GetAccountInfo()
+#deposit(2000,'true')
+#withdraw(500)
+#getBalance()
+
+#Create a SavingsAccount class that inherits from the account with the following
+#  attributes (Use Super () to pass valued to the base class):
+#MinBalance
+
+#Add the following methods to get account information, withdraw, and deposit:
+#getsavingAccountInfo()
+#deposit(2000,'true')
+#withdraw(500)
+#getBalance()
+
+#Validate MinBalance before allowing withdrawals.
+#Create a class that runs the program and accepts input from the end user to create
+#respective class objects and print details. Add a method to perform deposit and 
+#withdrawal transaction based on the end user input.
